61d3f85b5111cdb3e3def3dd4aa2cf5bc217f24a
EnTIv <1522935501@qq.com>
2020-10-27 21:02:06 +0800

    新增测试代码, 一堆小调整

--- src/main/java/com/sakuratown/sakuraminions/PlayerListener.java
+++ src/main/java/com/sakuratown/sakuraminions/PlayerListener.java
@@ -2,8 +2,6 @@ package com.sakuratown.sakuraminions;
 
 import com.sakuratown.sakuraminions.minions.Minion;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -11,15 +9,15 @@ import org.bukkit.inventory.ItemStack;
 
 public class PlayerListener implements Listener {
 
+    @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
 
-        System.out.println("AAA");
         ItemStack item = event.getItem();
         if (item == null) return;
 
         if (item.getType() == Material.EGG) {
 
-            Minion minion = new Minion("miner", 10, 9);
+            Minion minion = new Minion("miner", 9, 50);
             minion.openInventory(event.getPlayer());
         }
     }
