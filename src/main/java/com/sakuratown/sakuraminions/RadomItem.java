4287cc2b498013e8b26c3f6101603cff30f9e6ba
Iseason <iseasongit config --global user.emailgit config --global user.emailgit config --global user.email@outlook.com>
2020-10-27 18:52:44 +0800

    加权随机

--- src/main/java/com/sakuratown/sakuraminions/RadomItem.java
+++ src/main/java/com/sakuratown/sakuraminions/RadomItem.java
@@ -3,13 +3,13 @@ import java.util.Map;
 
 public class RadomItem {
     public String getRandomString(Map<String, Double> itemList) {
-        double total = 0.0,set = 0.0;
+        double total = 0.0,set = 0.0; //total 加权值之合
         for (Map.Entry<String, Double> item : itemList.entrySet()) {
             total += item.getValue();
         }
         double randomNum = Math.random() * total;
         for (Map.Entry<String, Double> item : itemList.entrySet()) {
-            set += item.getValue();
+            set += item.getValue();   //分阶段判断随机区间是否匹配
             if(randomNum <= set){
                 return item.getKey();
             }
