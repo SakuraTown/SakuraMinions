333842fa1350bf792af4b86152d77d131a31a2fe
Iseason <iseasongit config --global user.emailgit config --global user.emailgit config --global user.email@outlook.com>
2020-11-02 23:10:36 +0800

    配置文件修改

--- src/main/java/com/sakuratown/sakuraminions/minions/Config.java
+++ src/main/java/com/sakuratown/sakuraminions/minions/Config.java
@@ -34,7 +34,7 @@ public class Config {
     private static void menuLoad() {
         menuSection = config.getConfigurationSection("Menu");
         menuStyle = menuSection.getString("Style");
-        minionSection = config.getConfigurationSection("Minions.");
+        minionSection = config.getConfigurationSection("Minions");
     }
 
 
