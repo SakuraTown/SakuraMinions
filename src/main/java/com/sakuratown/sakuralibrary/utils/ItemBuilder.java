72e9ae738f1289d368cee412751cd4eb27c7858e
Iseason <iseasongit config --global user.emailgit config --global user.emailgit config --global user.email@outlook.com>
2020-11-07 18:22:46 +0800

    增加构建玩家头颅的功能

--- src/main/java/com/sakuratown/sakuralibrary/utils/ItemBuilder.java
+++ src/main/java/com/sakuratown/sakuralibrary/utils/ItemBuilder.java
@@ -1,13 +1,19 @@
 package com.sakuratown.sakuralibrary.utils;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class ItemBuilder {
 
@@ -103,5 +109,25 @@ public class ItemBuilder {
     public ItemStack build() {
         return itemStack;
     }
+    public ItemStack BuildSkull(String url) {
+        ItemStack head = itemStack;
+        if(head.getType() != Material.PLAYER_HEAD){return head;}
+        if (url.isEmpty()) return head;
 
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+
+        profile.getProperties().put("textures", new Property("textures", url));
+
+        try {
+            Method mtd = skullMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+            mtd.setAccessible(true);
+            mtd.invoke(skullMeta, profile);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
+            ex.printStackTrace();
+        }
+
+        head.setItemMeta(skullMeta);
+        return head;
+    }
 }
